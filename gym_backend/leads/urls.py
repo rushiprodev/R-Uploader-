0de87611bd3f74ed4ef86610107a5b68da5481f3
Rushikesh Bhagwat <146852834+rushiprodev@users.noreply.github.com>
2025-07-17 09:56:28 +0530

    Delete gym_backend directory

--- gym_backend/leads/urls.py
+++
@@ -1,9 +0,0 @@
-# gym_backend/leads/urls.py
-
-from django.urls import path
-from .views import create_lead  # Only import create_lead
-
-urlpatterns = [
-    # No path for a general homepage here, only the API endpoint
-    path('api/create-lead/', create_lead, name='create_lead'), # Ensure 'name' is consistent if used elsewhere
-]
